bd496e81a6b1b28c1af9b918b31332a627f77a65
phiphyzhou <phiphy.zhou@yahoo.com>
2016-01-12 17:41:24 -0500

    rnn modification

--- src/datareader.py
+++ src/datareader.py
@@ -9,7 +9,8 @@ import numpy as np
 num_files = 2 # number of dcd files we want to analyze 
 dcd_path = "/protein-data/bpti-all/bpti-all-"
 pdb_file = "/protein-data/bpti-all.pdb"
-window_size = 10
+window_size = 10 # number of frames to be averaged
+seq_size = 100  # number of averaged frames in a sequence
 
 def dcd_raw_data():
    
@@ -28,11 +29,15 @@ def dcd_raw_data():
     traj = md.load(dcd_files,top=pdb_file)
 
     # make window averages
-    coords = []
-    for i in xrange(0,traj.n_frames,window_size):
-        xyz_ave = np.mean(traj[i:i+window_size].xyz,axis=0)
-        coords.append(xyz_ave)
-    return coords
+    data = []
+    for i in xrange(0,traj.n_frames,window_size*seq_size):
+        coords = []
+        for j in xrange(i,i+window_size*seq_size, window_size):
+            xyz_ave = np.mean(traj[i:i+window_size].xyz,axis=0)
+            xyz_ave = xyz_ave.flatten()
+            coords.append(xyz_ave) # one sequence
+        data.append(coords) 
+    return data
 
 if __name__=="__main__":
     traj = dcd_raw_data()
--- src/protein_rnn.py
+++ src/protein_rnn.py
@@ -3,38 +3,70 @@ Use rnn to learn features from trajectory data
 '''
 
 import tensorflow.python.platform
+import tensorflow as tf
+
+import time
+
+import numpy as np
+
 import rnn
 import rnn_cell
+import datareader
+
 from tensorflow.python.ops.rnn_cell import *
 from tensorflow.python.ops.rnn import *
 
+tf.flags.DEFINE_string("data_path", None, "data_path")
+FLAGS = tf.flags.FLAGS
+
 class RNNModel(object):
-    ''' The gated recurrent unit rnn model '''
+    ''' The basic rnn model '''
     def __init__(self, is_training, config):
-        self.batch_size = batch_size = config.batch_size
-        self.num_steps = num_steps = config.num_steps
-        size = config.hidden_size
-        cell = rnn_cell.GRUCell(num_units)
+        self.batch_size = batch_size = config.batch_size # size for mini batch training
+        self.num_steps = num_steps = config.num_steps # maximum number of training iteration?
+        size = config.hidden_size # state size
+        feature_size = config.feature_size
+
+        self._input_data = tf.placeholder(tf.int32, [batch_size, num_steps])
+        self._targets = tf.placeholder(tf.int32, [batch_size, num_steps])
+
+        basic_cell = rnn_cell.BasicLSTMCell(size)
+        if is_training and config.keep_prob < 1: # use dropout 
+            basic_cell = rnn_cell.DropoutWrapper(
+                basic_cell, output_keep_prob=config.keep_prob)
+        cell = rnn_cell.MultiRNNCell(  
+                [basic_cell] * config.num_layers) # multiple layers
+        self._initial_state = cell.zero_state(batch_size, tf.float32)
+
+        with tf.device("/cpu:0"):
+            embedding = tf.get_variable("embedding", [feature_size, size])
+            inputs = tf.nn.embedding_lookup(embedding, self._input_data)
+
+        if is_training and config.keep_prob < 1:
+            inputs = tf.nn.dropout(inputs, config.keep_prob)
 
 class SmallConfig(object):
-    """Small config."""
+    """Small config for training"""
     init_scale = 0.1
-    batch_size = 20
+    batch_size = 10
     learning_rate = 1.0
     max_grad_norm = 5
-    num_layers = 2
-    num_steps = 20
+    num_layers = 1
+    num_steps = 5
     hidden_size = 200
     max_epoch = 4
     max_max_epoch = 13
     keep_prob = 1.0
     lr_decay = 0.5
-    vocab_size = 10000
+    feature_size = 10000
 
 def main(unused_args):
+    if not FLAGS.data_path:
+        raise ValueError("Must set --data_path to PTB data directory")
     config = SmallConfig()
     with tf.Graph().as_default(), tf.Session() as session:
         m = RNNModel(True,config)
+    print "done"
 
 if __name__ == "__main__":
    tf.app.run() 
